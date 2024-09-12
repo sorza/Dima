16ccb5cacfb7e85644ea5e6791a993b4edf2d9f5
sorza <alexandre.sorza@outlook.com>
2024-09-11 23:03:30 -0300

    esboço tela de login

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -1,9 +1,22 @@
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+namespace Dima.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+           
+            builder.Services.AddMudServices();
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
