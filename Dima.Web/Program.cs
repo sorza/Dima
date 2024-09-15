07defea7c3ca09128d72dcf93338efc86d19a43b
sorza <alexandre.sorza@outlook.com>
2024-09-15 19:17:18 -0300

    Configurando rota para o backendUrl, registrando os serviços de autenticação e adicionando authorizeviewroute no app.razor

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -1,3 +1,5 @@
+using Dima.Web.Security;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -9,9 +11,18 @@ namespace Dima.Web
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            Configuration.BackEndUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
             
+            builder.Services.AddScoped<CookieHandler>();
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+            builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
+
             builder.Services.AddMudServices();
 
             builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
--- Dima.Web/Security/CookieHandler.cs
+++ Dima.Web/Security/CookieHandler.cs
@@ -1,7 +1,16 @@
-﻿namespace Dima.Web.Security
-{
-    public class CookieHandler
-    {
+﻿
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
+namespace Dima.Web.Security
+{
+    public class CookieHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+            request.Headers.Add("X-Requested-With", ["XMLHttpRequest"]);
+
+            return base.SendAsync(request, cancellationToken);
+        }
     }
 }
