0dea4e911e020d662a0d092e890f02c77ee492cb
sorza <alexandre.sorza@outlook.com>
2024-09-16 23:51:34 -0300

    Implementação das Pages CreateCategory e ListCategory

---
+++ Dima.Web/Pages/Categories/List.razor.cs
@@ -0,0 +1,54 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Categories
+{
+    public partial class ListCategoriesPage : ComponentBase
+    {
+        #region Properties
+
+        public bool IsBusy { get; set; } = false;
+        public List<Category> Categories { get; set; } = [];
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public ICategoryHandler Handler { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task OnInitializedAsync()
+        {
+           IsBusy = true;
+
+            try
+            {
+                var request = new GetAllCategoriesRequest();
+                var result = await Handler.GetAllAsync(request);
+
+                if (result.IsSuccess)
+                    Categories = result.Data ?? [];
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
+    }
+}
