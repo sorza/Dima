00ef1432a04719bd67c30c7799c45ed75d1b5fac
sorza <alexandre.sorza@outlook.com>
2024-09-29 21:00:21 -0300

    Mapping de Voucher

--- Dima.Core/Models/Voucher.cs
+++ Dima.Core/Models/Voucher.cs
@@ -6,7 +6,7 @@
         public string Number { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public bool IsActibe { get; set; }
+        public bool IsActive { get; set; }
         public decimal Amount { get; set; }
     }
 }
