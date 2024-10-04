75295333ecbba136f32ec051c59568e2fbb96da1
sorza <alexandre.sorza@outlook.com>
2024-10-03 23:19:58 -0300

    Implementando VoucherHandler

--- Dima.Web/Handlers/VoucherHandler.cs
+++ Dima.Web/Handlers/VoucherHandler.cs
@@ -2,14 +2,15 @@
 using Dima.Core.Models;
 using Dima.Core.Requests.Orders;
 using Dima.Core.Responses;
+using System.Net.Http.Json;
 
 namespace Dima.Web.Handlers
 {
-    public class VoucherHandler : IVoucherHandler
+    public class VoucherHandler(IHttpClientFactory httpClientFactory) : IVoucherHandler
     {
-        public Task<Response<Voucher?>> GetByNumberAsync(GetVoucherByNumberRequest request)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+        public async Task<Response<Voucher?>> GetByNumberAsync(GetVoucherByNumberRequest request)
+            => await _client.GetFromJsonAsync<Response<Voucher?>>($"v1/vouchers/{request.Number}")
+            ?? new Response<Voucher?>(null, 400, "Não foi possível obter o voucher.");
     }
 }
