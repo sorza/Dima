37ed2e84ef3cffb42a83fc90a42e88250a92cbc9
sorza <alexandre.sorza@outlook.com>
2024-10-27 22:08:42 -0300

    Criando uma sessao com Stripe

---
+++ Dima.Api/Handlers/StripeHandler.cs
@@ -0,0 +1,63 @@
+﻿using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Stripe;
+using Dima.Core.Responses;
+using Dima.Core.Responses.Stripe;
+using Stripe;
+using Stripe.Checkout;
+
+namespace Dima.Api.Handlers
+{
+    public class StripeHandler : IStripeHandler
+    {
+        public async Task<Response<string?>> CreateSessionAsync(CreateSessionRequest request)
+        {
+            var options = new SessionCreateOptions
+            {
+                CustomerEmail = request.UserId,
+                PaymentIntentData = new SessionPaymentIntentDataOptions
+                {
+                    Metadata = new Dictionary<string, string>
+                    {
+                        { "order", request.OrderNumber }
+                    }
+                },
+                PaymentMethodTypes =
+                [
+                    "card"
+                ],
+                LineItems =
+                [
+                   new SessionLineItemOptions
+                   {
+                       PriceData = new SessionLineItemPriceDataOptions
+                       {
+                           Currency = "BRL",
+                           ProductData = new SessionLineItemPriceDataProductDataOptions
+                           {
+                               Name = request.ProductTitle,
+                               Description = request.ProductDescription
+                           },
+                           UnitAmount = request.OrderTotal
+                       },
+                       Quantity = 1
+                   }
+                ],
+                Mode = "payment", 
+                SuccessUrl = $"{Configuration.FrontendUrl}/pedidos/{request.OrderNumber}/confirmar",
+                CancelUrl = $"{Configuration.FrontendUrl}/pedidos/{request.OrderNumber}/cancelar"
+
+            };
+
+            var service = new SessionService();
+            var session = await service.CreateAsync(options);
+
+            return new Response<string?>(session.Id);
+        }
+
+        public Task<Response<List<StripeTransactionResponse>>> GetTransactionsByOrderNumberAsunc(GetTransactionsByOrderNumberRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
